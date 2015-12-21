60c770fc922d87b7f4c1a30bd4ca54862fd3623e
Orelab <aurelien.chirot@idee-lab.fr>
2015-12-21 19:52:41 +0100

    adding some missing files for translations.

---
+++ application/language/english/el_cms_lang.php
@@ -0,0 +1,170 @@
+<?php defined("BASEPATH") OR exit("No direct script access allowed");
+
+
+//-- Index
+
+$lang["cms_index"] = "
+		<h1>Election libre</h1>
+		
+		<h2><b>Organize</b> your elections on the internet safely, seamlessly.</h2>
+		<h3><b>Décide</b> democratically, one vote per elector.</h3>
+		<h3><b>Vote</b> with secret ballot, anonymously.</h3>
+";
+
+
+
+//-- New election (public form)
+
+$lang["cms_manage_index"] = "
+		<legend>An elegant weapon for a more civilized age</legend>
+		
+		<p>Que vous soyez organisateur des prochaines élections de votre association,
+		que vous ayez en tête de lancer un référendum dans votre entreprise, ou plus simplement
+		que vous souhaitiez lancer un questionnaire aux abonnés de votre blog, ou toutes autres
+		situations nécessitant de prendre une décision en collectif, vous allez devoir vous lancer
+		dans l'organisation d'une élection qui nécessite à la fois logistique contraignante et
+		nécessité de rassurer les participants.</p>
+		
+		<p>Notre système est à la fois simple et ouvert. En quelques minutes, vous pourrez
+		créer une élection. Chaque électeur recevra ensuite par email une invitation à voter
+		de manière totalement sécurisée et transparente. Les résultats seront ensuite rendus
+		publics sur le site, et chaque électeur sera tenu informé par email.</p>
+		
+		<ul>		
+			<li><b>Transparence totale</b> : le code source est public.</li>
+			<li><b>Suffrages anonymes</b> : mécanismes de contrôle inclus.</li>
+		</ul>
+";
+
+$lang["cms_manage_start"] = "Start";
+
+$lang["cms_manage_info"] = "Please fill-in theses information if we need to contact you, just in case of problem.";
+
+$lang["cms_manage_contact"] = "You contact information";
+
+$lang["cms_manage_name"] = "Name";
+
+$lang["cms_manage_surname"] = "Surname";
+
+$lang["cms_manage_email"] = "Email";
+
+$lang["cms_manage_business"] = "Votre business";
+
+$lang["cms_manage_business_detail"] = "The name of your enterprise, association, group...";
+
+$lang["cms_manage_prev"] = "Back";
+
+$lang["cms_manage_next"] = "Next";
+
+$lang["cms_manage_election"] = "
+		<legend>Message to the electors</legend>
+		
+		<p>This message will be sent to the electors as an invitation to vote.</p>
+";
+
+$lang["cms_manage_title"] = "Title";
+
+$lang["cms_manage_msg"] = "Message";
+
+$lang["cms_manage_planification"] = "Planify the elections";
+
+$lang["cms_manage_begining"] = "Start voting";
+
+$lang["cms_manage_ending"] = "End voting";
+
+$lang["cms_manage_candidates"] = "
+		<legend>The candidates</legend>
+		
+		<p>Please, enter the candidates now. You can also write lists.</p>
+";
+
+$lang["cms_manage_winners"] = "
+		<legend>Number of winners</legend>
+		
+		<p>How much winners from those who gain the maximum votes will be declared ?</p>
+";
+
+$lang["cms_manage_electors"] = "
+		<legend>The electors</legend>
+		
+		<p>Please, download the list of electors, or write-it below. If you choose to download
+		the list, you will find a sample file here. Please, DON'T change the file type nor the
+		format of the document !</p>
+";
+
+$lang["cms_manage_template"] = "template file";
+
+$lang["cms_manage_thanks"] = "
+		<legend>Ready ?</legend>
+		
+		<p>Your elections are ready to be published and will start at the date you previously
+		mentionned. Just click, the SAVE button. You will next received a confirmation mail
+		with your pesonal admin access.</p>
+";
+
+$lang["cms_manage_save"] = "Save";
+
+
+
+
+//-- Credits
+
+$lang["cms_credits_title"] = "Credits&nbsp;:";
+
+$lang["cms_credits_framework"] = "Framework&nbsp;:";
+
+$lang["cms_credits_developer"] = "Developer&nbsp;:";
+
+$lang["cms_credits_translation"] = "Translations&nbsp;:";
+
+$lang["cms_credits_design"] = "Design&nbsp;:";
+
+
+
+//-- Disclaimer
+
+$lang["cms_credits_disclaimer"] = "
+	<h2>Avertissement</h2>
+	
+	<p>Le vote électronique est un sujet délicat : il est parfois sujet à
+	méfiance, de par son côté obscur et complexe. Cette méfiance est légitime :
+	dans l'absolu, tout système informatique est piratable.</p>
+	
+	<h3>Ce que nous cherchons à améliorer :</h3>
+	
+	<ul>
+		<li><b>La transparence :</b> en publiant le code source du programme, en cherchant à le rendre simple,
+		et en fournissant une version en ligne utilisable</li>
+		<li><b>La confidentialité :</b> le programme enregistre, d'une part, le fait qu'un électeur à voté 
+		(cela est nécessaire pour s'assurer qu'un seul vote est effectué par électeur), et enregistre 
+		d'autre part le vote, sans toutefois enregistrer qui a voté pour quoi.</li>
+		<li><b>L'accessibilité :</b> en utilisant le réseau des réseaux, et en fournissant une interface
+		simple, adaptée au périphériques mobiles, et répondants aux normes d'accessibilité, nous nous assurons
+		qu'un maximum d'électeurs peuvent utiliser le système.</li>
+		<li><b>La vérifiabilité :</b> chaque électeur peut - à l'aide de sa clé personnelle - vérifier son
+		bulletin de vote, à tout moment, pendant et après le dépouillement des résultats.</li>
+	</ul>
+	
+	<h3>Ce que nous ne garantissons pas :</h3>
+	
+	<ul>
+		<li><b>L'identité de l'électeur :</b> chaque électeur est identifié par son adresse email.
+		Si cette adresse est espionnée ou piratée, la légitimité du vote est compromise.</li>
+		<li><b>La coercition :</b> si le vote de l'électeur est réalisé sans une juste intention,
+		sous la contrainte, ou avec des intérêts mercantiles, le système informatique ne peut garantir la 
+		validité des suffrages. Une fraude de type 
+		\"<a href=\"http://binaire.blog.lemonde.fr/2015/08/24/attaque-a-litalienne-2/\">attaque à l'italienne</a>\"
+		est notamment possible avec ce système.</li>
+	</ul>
+	
+	<h3>Le vote électronique, pour quel usages ?</h3>
+	
+	<p>Nous avons conçu ce programme pour répondre au besoin d'organiser simplement - et de manière démocratique - 
+	une élection ou un référundum dans le cadre d'un groupe restreint et dont les motivations n'est pas excessif,
+	comme dans le cadre d'une association caritative qui ne brasse pas assez de finances pour atiser les convoitises.</p>
+";
+
+
+
+
+
---
+++ application/language/english/el_javascript_lang.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+$lang['js_error'] = "Sorry, an unexpected error occured.";
+$lang['js_error_empty_field'] = 'Please, fill-in all the fields.';
+$lang['js_error_malformed_data'] = 'Invalid data';
+$lang['js_error_invalid_email'] = 'Please, fill-in a valid email address.';
+
+$lang['js_confirm_row_delete'] = 'Are your sur you want to drop with elector ?';
+$lang['js_name'] = 'Name';
+$lang['js_surname'] = 'Surname';
+$lang['js_email'] = 'Email';
+
+$lang['js_enter_email'] = 'Please, enter your email address :';
+$lang['js_confirm_renew_invitation'] = 'You should have received a new invitation '
+													. 'with your IDs. Please visit your mailbox now.';
